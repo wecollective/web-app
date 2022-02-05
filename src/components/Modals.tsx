b9a786012dba1822ec36c1f6a22739884a8ba379
jhweir <james@weco.io>
2022-02-05 17:42:38 +0000

    New ImageUploadModal, react-images-upload removed

--- src/components/Modals.tsx
+++ src/components/Modals.tsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react'
 import { AccountContext } from '@contexts/AccountContext'
 import AlertModal from '@src/components/modals/AlertModal'
 import NavbarDropDownModal from '@src/components/modals/NavbarDropDownModal'
-import ImageUploadModal from '@src/components/modals/ImageUploadModal'
 import CreatePostModal from '@src/components/modals/CreatePostModal'
 import ResetPasswordModal from '@src/components/modals/ResetPasswordModal'
 import LogInModal from '@src/components/modals/LogInModal'
@@ -20,7 +19,6 @@ const Modals = (): JSX.Element => {
         forgotPasswordModalOpen,
         setForgotPasswordModalOpen,
         navBarDropDownModalOpen,
-        imageUploadModalOpen,
         createPostModalOpen,
         donateModalOpen,
         setDonateModalOpen,
@@ -37,7 +35,6 @@ const Modals = (): JSX.Element => {
             )}
             {donateModalOpen && <DonateModal close={() => setDonateModalOpen(false)} />}
             {navBarDropDownModalOpen && <NavbarDropDownModal />}
-            {imageUploadModalOpen && <ImageUploadModal />}
             {createPostModalOpen && <CreatePostModal />}
             {resetPasswordModalOpen && <ResetPasswordModal />}
         </>
--- src/components/Modals/ImageUploadModal.tsx
+++ src/components/Modals/ImageUploadModal.tsx
@@ -1,84 +1,125 @@
-import React, { useContext, useState } from 'react'
-import ImageUploader from 'react-images-upload'
+import React, { useState } from 'react'
 import axios from 'axios'
 import Cookies from 'universal-cookie'
-import { AccountContext } from '@contexts/AccountContext'
-import { SpaceContext } from '@contexts/SpaceContext'
-import { UserContext } from '@contexts/UserContext'
-import styles from '@styles/components/ImageUploadModal.module.scss'
+import styles from '@styles/components/modals/ImageUploadModal.module.scss'
 import config from '@src/Config'
 import Button from '@components/Button'
 import Modal from '@components/Modal'
-import Column from '@components/Column'
 import Row from '@components/Row'
-import CloseButton from '../CloseButton'
-import CloseOnClickOutside from '../CloseOnClickOutside'
+import Input from '@components/Input'
 
-const ImageUploadModal = (): JSX.Element => {
-    const { accountData, setImageUploadModalOpen, imageUploadType, getAccountData } = useContext(AccountContext)
-    const { spaceData, getSpaceData } = useContext(SpaceContext)
-    const { getUserData } = useContext(UserContext)
-    const [image, setImage] = useState<File[]>([])
-
+const ImageUploadModal = (props: {
+    type: 'space-flag' | 'space-cover' | 'user-flag' | 'user-cover' | 'gbg-topic' | 'gbg-background'
+    id: number
+    title: string
+    subTitle?: string
+    mbLimit?: number
+    onSaved?: (imageURL: string) => void
+    close: () => void
+}): JSX.Element => {
+    const { type, id, title, subTitle, mbLimit, onSaved, close } = props
+    const [imageFile, setImageFile] = useState<File>()
+    const [imageURL, setImageURL] = useState('')
+    const [imagePreviewURL, setImagePreviewURL] = useState('')
+    const [imageSizeError, setImageSizeError] = useState(false)
+    const [loading, setLoading] = useState(false)
     const cookies = new Cookies()
 
+    function selectImageFile() {
+        const input = document.getElementById('file-input') as HTMLInputElement
+        if (input && input.files && input.files[0]) {
+            setImageURL('')
+            if (input.files[0].size > mbLimit! * 1024 * 1024) {
+                setImageSizeError(true)
+                setImagePreviewURL('')
+                setImageFile(undefined)
+                input.value = ''
+            } else {
+                setImageSizeError(false)
+                setImageFile(input.files[0])
+                setImagePreviewURL(URL.createObjectURL(input.files[0]))
+            }
+        }
+    }
+
+    function selectImageURL(url) {
+        setImageSizeError(false)
+        setImageURL(url)
+        setImagePreviewURL(url)
+        setImageFile(undefined)
+        const input = document.getElementById('file-input') as HTMLInputElement
+        if (input) input.value = ''
+    }
+
     function saveImage() {
-        const holonQuery = `?holonId=${spaceData && spaceData.id}`
+        setLoading(true)
         const accessToken = cookies.get('accessToken')
-        const formData = new FormData()
-        formData.append('image', image[0])
+        const options = { headers: { Authorization: `Bearer ${accessToken}` } }
+        let data
+        if (imageURL) data = { imageURL }
+        else {
+            data = new FormData()
+            data.append('image', imageFile)
+            options.headers['Content-Type'] = 'multipart/form-data'
+        }
         axios
-            .create({
-                baseURL: config.apiURL,
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                    'Access-Control-Allow-Origin': '*',
-                },
-            })
-            .post(`${config.apiURL}/${imageUploadType}-upload${holonQuery}`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    // 'Access-Control-Allow-Origin': '*'
-                },
-            })
+            .post(`${config.apiURL}/image-upload?type=${type}&id=${id}`, data, options)
             .then((res) => {
-                if (res.data === 'success') {
-                    setImageUploadModalOpen(false)
-                    // todo: update context directly
-                    // setTimeout(() => {
-                    //     if (imageUploadType.includes('holon')) {
-                    //         getSpaceData()
-                    //         getAccountData()
-                    //     }
-                    //     if (imageUploadType.includes('user')) {
-                    //         getUserData(accountData.id)
-                    //         getAccountData()
-                    //     }
-                    // }, 200)
-                }
+                if (onSaved) onSaved(imageURL || res.data.imageURL)
+                setLoading(false)
+                close()
             })
+            .catch((error) => console.log(error))
     }
 
     return (
-        <Modal close={() => setImageUploadModalOpen(false)} centered>
-            <ImageUploader
-                withIcon
-                withPreview
-                buttonText='Choose images'
-                onChange={(imageFile) => setImage(imageFile)}
-                imgExtension={['.jpg', '.jpeg', '.gif', '.png']}
-                maxFileSize={1 * 1024 * 1024}
-                singleImage
+        <Modal close={close} centered>
+            <h1>{title}</h1>
+            {subTitle && <p>{subTitle}</p>}
+            {imagePreviewURL && (
+                <img
+                    id='image-preview'
+                    className={styles.imagePreview}
+                    src={imagePreviewURL}
+                    alt=''
+                />
+            )}
+            {imageSizeError && <p>Image too large. Max size: {mbLimit}MB</p>}
+            <Row className={styles.fileUploadInput}>
+                <label htmlFor='file-input'>
+                    {imageFile ? 'Change' : 'Upload'} image
+                    <input
+                        type='file'
+                        id='file-input'
+                        accept='.png, .jpg, .jpeg, .gif'
+                        onChange={selectImageFile}
+                        hidden
+                    />
+                </label>
+            </Row>
+            <p>or paste an image URL:</p>
+            <Input
+                type='text'
+                placeholder='image url...'
+                value={imageURL}
+                onChange={(url) => selectImageURL(url)}
+                style={{ marginBottom: 30 }}
             />
             <Button
                 text='Save image'
                 color='blue'
-                disabled={!image[0]}
-                style={{ marginRight: 10 }}
+                disabled={!imageURL && !imageFile}
+                loading={loading}
                 onClick={saveImage}
             />
         </Modal>
     )
+}
+
+ImageUploadModal.defaultProps = {
+    subTitle: null,
+    mbLimit: 2,
+    onSaved: null,
 }
 
 export default ImageUploadModal
