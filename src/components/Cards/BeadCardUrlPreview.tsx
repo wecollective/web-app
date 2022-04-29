61d042f51af016f3bda98b4af45bf375a53ebac9
jhweir <james@weco.io>
2022-04-29 16:40:51 +0100

    BeadCardUrlPreview set up

---
+++ src/components/Cards/BeadCardUrlPreview.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import styles from '@styles/components/cards/BeadCardUrlPreview.module.scss'
+import Row from '@components/Row'
+import Column from '@components/Column'
+import { ReactComponent as LinkIconSVG } from '@svgs/link-solid.svg'
+
+const BeadCardUrlPreview = (props: {
+    url: string
+    image: string | null
+    domain: string | null
+    title: string | null
+    description: string | null
+    style?: any
+}): JSX.Element => {
+    const { url, image, domain, title, description, style } = props
+
+    const availableMetaData = image || domain || title || description
+
+    function handleImageError(e) {
+        e.target.onerror = null
+        if (!e.target.src.includes('//images.weserv.nl/')) {
+            e.target.src = `//images.weserv.nl/?url=${image}`
+        } else {
+            e.target.src = ''
+        }
+    }
+
+    return (
+        <a className={styles.wrapper} style={style} href={url} target='_blank' rel='noreferrer'>
+            {image && (
+                <img src={image} onError={(e) => handleImageError(e)} aria-label='URL image' />
+            )}
+            <Column>
+                {availableMetaData ? (
+                    <Column className={styles.metaData}>
+                        {title && <h1>{title}</h1>}
+                        {description && <p>{description}</p>}
+                        {domain && (
+                            <Row centerY centerX className={styles.domain}>
+                                <LinkIconSVG />
+                                <p>{domain}</p>
+                            </Row>
+                        )}
+                    </Column>
+                ) : (
+                    <Column style={{ padding: 10 }}>
+                        <p style={{ fontWeight: 800 }}>{url}</p>
+                    </Column>
+                )}
+            </Column>
+        </a>
+    )
+}
+
+BeadCardUrlPreview.defaultProps = {
+    style: null,
+}
+
+export default BeadCardUrlPreview
