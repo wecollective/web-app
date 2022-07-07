6a6aea229c7cd2bd8d4e0d0c0198709396ee9055
jhweir <james@weco.io>
2022-07-07 18:49:48 +0100

    CheckBox component set up

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import styles from '@styles/components/CheckBox.module.scss'
+import { ReactComponent as CheckIcon } from '@svgs/check-solid.svg'
+
+const CheckBox = (props: {
+    text: string
+    checked: boolean
+    onChange: (checked: boolean) => void
+    disabled?: boolean
+    style?: any
+}): JSX.Element => {
+    const { text, checked, onChange, disabled, style } = props
+    const id = uuidv4()
+    return (
+        <label
+            htmlFor={id}
+            className={`${styles.wrapper} ${disabled && styles.disabled}`}
+            style={style}
+        >
+            <input
+                type='checkbox'
+                id={id}
+                checked={checked}
+                onChange={(e) => onChange(e.target.checked)}
+                disabled={disabled}
+            />
+            <div />
+            <CheckIcon />
+            <p>{text}</p>
+        </label>
+    )
+}
+
+CheckBox.defaultProps = {
+    disabled: false,
+    style: null,
+}
+
+export default CheckBox
