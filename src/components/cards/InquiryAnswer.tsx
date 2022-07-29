051feded73f36de7c2deb2af22e07a58449ccda8
jhweir <james@weco.io>
2022-07-29 18:15:38 +0100

    InquiryAnswer updates for weighted choice

--- src/components/cards/InquiryAnswer.tsx
+++ src/components/cards/InquiryAnswer.tsx
@@ -1,27 +1,37 @@
 import CheckBox from '@components/CheckBox'
 import CloseButton from '@components/CloseButton'
+import Input from '@components/Input'
 import Row from '@components/Row'
 import styles from '@styles/components/cards/InquiryAnswer.module.scss'
 import React from 'react'
 
 const InquiryAnswer = (props: {
     index: number
+    type: 'single-choice' | 'multiple-choice' | 'weighted-choice'
     answer: any
     color: string
     preview?: boolean
     selected?: boolean
-    onChange?: (selected: boolean) => void
+    onChange?: (value: boolean | number) => void
     close?: () => void
 }): JSX.Element => {
-    const { index, answer, color, preview, selected, onChange, close } = props
+    const { index, type, answer, color, preview, selected, onChange, close } = props
 
     return (
         <Row centerY className={styles.wrapper}>
-            {!preview && (
+            {!preview && ['single-choice', 'multiple-choice'].includes(type) && (
                 <CheckBox
                     checked={selected || false}
                     onChange={(v) => onChange && onChange(v)}
                     style={{ marginRight: 10 }}
+                />
+            )}
+            {!preview && type === 'weighted-choice' && (
+                <Input
+                    type='text'
+                    value={answer.accountPoints}
+                    onChange={(v) => onChange && onChange(+v.replace(/\D/g, ''))}
+                    style={{ width: 60 }}
                 />
             )}
             <div style={{ backgroundColor: color }}>
