d63fc6fb0864801e54a168145b7e4d3bbdaf1207
jhweir <james@weco.io>
2022-08-02 15:57:07 +0100

    Style prop added to CloseButton component

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -2,19 +2,23 @@ import styles from '@styles/components/CloseButton.module.scss'
 import { ReactComponent as TimesIconSVG } from '@svgs/times-solid.svg'
 import React from 'react'
 
-const CloseButton = (props: { size: number; onClick: () => void }): JSX.Element => {
-    const { size, onClick } = props
+const CloseButton = (props: { size: number; onClick: () => void; style?: any }): JSX.Element => {
+    const { size, onClick, style } = props
 
     return (
         <button
             className={styles.closeButton}
             type='button'
             onClick={onClick}
-            style={{ width: size, height: size }}
+            style={{ ...style, width: size, height: size }}
         >
             <TimesIconSVG width={size} height={size} />
         </button>
     )
+}
+
+CloseButton.defaultProps = {
+    style: null,
 }
 
 export default CloseButton
