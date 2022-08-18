852327aa5c734e0400d447e2a1d0930fb5c464d1
jhweir <james@weco.io>
2022-08-18 22:36:26 +0100

    Governance page tweaks

--- src/pages/SpacePage/SpacePageGovernance.tsx
+++ src/pages/SpacePage/SpacePageGovernance.tsx
@@ -18,42 +18,50 @@ const SpacePageGovernance = (): JSX.Element => {
                 <p>Space data loading... </p>
             ) : (
                 <Column className={styles.content}>
-
                     <h1>Still to be developed.</h1>
                     <p>
                         This section will contain polls and customisable governance modules to help
                         the community self-govern.
                     </p>
-                    <br /><br />
+                    <br />
+                    <br />
                     <h1>In the meantime...</h1>
-                    <p>You can learn more about our plans on our wiki:</p>
-                    <TextLink text='http://wiki.weco.io/#/governance' link='http://wiki.weco.io/#/governance'/>
-                    <a
-                     href='http://wiki.weco.io/#/governance'
-                    >
-                        <img 
-                            width='100%' 
-                            src='http://wiki.weco.io/images/decision-pipeline.jpg' 
-                            alt='Stages: Suggestions, Proposals, Pending Tasks, Active Tasks, Completed Tasks' 
+                    <p>
+                        You can learn more about our plans for governance on the platform on our
+                        wiki:
+                    </p>
+                    <a href='http://wiki.weco.io/#/governance' className='blueText'>
+                        http://wiki.weco.io/#/governance
+                    </a>
+                    <a href='http://wiki.weco.io/#/governance'>
+                        <img
+                            width='100%'
+                            src='http://wiki.weco.io/images/decision-pipeline.jpg'
+                            alt='Stages: Suggestions, Proposals, Pending Tasks, Active Tasks, Completed Tasks'
                         />
                     </a>
                     <br />
-                    <p>You can also contribute to our development conversations across each of these spaces:</p>
+                    <p>
+                        You can also contribute to our development conversations across each of
+                        these spaces:
+                    </p>
                     <br />
-                    <TextLink text='s/weco-governance' link='https://weco.io/s/weco-governance/posts' />
+                    <TextLink text='Governance' link='/s/weco-governance/posts' />
                     <br />
-                    <TextLink text='s/weco-feedback' link='https://weco.io/s/weco-feedback/posts' />
+                    <TextLink text='Feedback' link='/s/weco-feedback/posts' />
                     <br />
-                    <TextLink text='s/weco-questions' link='https://weco.io/s/weco-questions/posts' />
+                    <TextLink text='Questions' link='/s/weco-questions/posts' />
                     <br />
-                    <TextLink text='s/weco-ideas' link='https://weco.io/s/weco-ideas/posts' />
+                    <TextLink text='Ideas' link='/s/weco-ideas/posts' />
                     <br />
-                    <TextLink text='s/weco-requests' link='https://weco.io/s/weco-requests/posts' />
-                    <br /><br /><br />
-                    <p>We'll be sharing platform updates here:</p>
+                    <TextLink text='Requests' link='/s/weco-requests/posts' />
                     <br />
-                    <TextLink text='s/weco-updates' link='https://weco.io/s/weco-updates/posts' />
-                    <div style={{ height: '6vh' }}/>
+                    <br />
+                    <br />
+                    <p>We&apos;ll be sharing platform updates here:</p>
+                    <br />
+                    <TextLink text='Updates' link='/s/weco-updates/posts' />
+                    <div style={{ height: '6vh' }} />
                 </Column>
             )}
         </Column>
