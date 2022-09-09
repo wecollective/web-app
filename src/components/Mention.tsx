c1f551592f2bd44cb66a4f5701df94f16f3bb82e
jhweir <james@weco.io>
2022-09-09 18:47:11 +0100

    Mention component updates

--- src/components/Mention.tsx
+++ src/components/Mention.tsx
@@ -1,28 +1,30 @@
+import ImageTitle from '@components/ImageTitle'
 import Row from '@components/Row'
 import styles from '@styles/components/Mention.module.scss'
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const MentionComponent = (props: { mention; children }): JSX.Element => {
     const { mention, children } = props
     const [modalOpen, setModalOpen] = useState(false)
-    const openPage = () => window.open(`/u/${mention.link}`, '_blank')
     return (
         <span className={styles.mentionComponent}>
-            <span
+            <Link
+                to={`/u/${mention.link}`}
                 className={styles.text}
-                role='button'
-                tabIndex={0}
-                onKeyDown={openPage}
-                onClick={openPage}
                 onMouseEnter={() => setModalOpen(true)}
                 onMouseLeave={() => setModalOpen(false)}
             >
                 @{children}
-            </span>
+            </Link>
             {modalOpen && (
-                <Row className={styles.modal} centerY>
-                    <img src={mention.avatar} alt='' />
-                    <p>u/{mention.link}</p>
+                <Row className={styles.modal}>
+                    <ImageTitle
+                        type='user'
+                        imagePath={mention.avatar}
+                        title={`u/${mention.link}`}
+                        shadow
+                    />
                 </Row>
             )}
         </span>
