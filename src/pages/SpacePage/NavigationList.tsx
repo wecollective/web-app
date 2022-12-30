d5269f89b5aed0edafc3032cde67f456d6bfc111
jhweir <james@weco.io>
2022-12-30 20:27:22 +0000

    NavigationLists recursive + clean up

--- src/pages/SpacePage/NavigationList.tsx
+++ src/pages/SpacePage/NavigationList.tsx
@@ -8,167 +8,189 @@ import config from '@src/Config'
 import styles from '@styles/pages/SpacePage/NavigationList.module.scss'
 import { ArrowDownIcon, ArrowUpIcon, MinusIcon, PlusIcon } from '@svgs/all'
 import axios from 'axios'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import Cookies from 'universal-cookie'
+
+const Space = (props: {
+    space: any
+    type: 'parent' | 'child'
+    selectedSubPage: string
+    expand: (space: any, type: 'parent' | 'child') => void
+    onLocationChange: (() => void) | undefined
+}): JSX.Element => {
+    const { space, type, selectedSubPage, expand, onLocationChange } = props
+    const { handle, name, flagImagePath, children, totalChildren, expanded, loading } = space
+    return (
+        <Column>
+            <Row centerY style={{ marginBottom: 10 }}>
+                <ImageTitle
+                    type='space'
+                    imagePath={flagImagePath}
+                    title={name}
+                    link={`/s/${handle}/${selectedSubPage}`}
+                    fontSize={14}
+                    imageSize={35}
+                    onClick={() => onLocationChange && onLocationChange()}
+                    wrapText
+                />
+                {totalChildren > 0 && (
+                    <button
+                        className={styles.expandButton}
+                        type='button'
+                        onClick={() => expand(space, type)}
+                    >
+                        {loading ? (
+                            <LoadingWheel size={20} style={{ marginLeft: 7 }} />
+                        ) : (
+                            <>{expanded ? <MinusIcon /> : <PlusIcon />}</>
+                        )}
+                    </button>
+                )}
+            </Row>
+            {expanded && (
+                <Column scroll className={styles.childSpaces}>
+                    {children &&
+                        children.map((child) => (
+                            <Space
+                                key={child.id}
+                                space={child}
+                                type={type}
+                                selectedSubPage={selectedSubPage}
+                                expand={expand}
+                                onLocationChange={onLocationChange}
+                            />
+                        ))}
+                </Column>
+            )}
+        </Column>
+    )
+}
 
 const NavigationList = (props: { onLocationChange?: () => void; style?: any }): JSX.Element => {
     const { onLocationChange, style } = props
-    const { spaceData, setSpaceData, selectedSpaceSubPage } = useContext(SpaceContext)
-    const { DirectParentSpaces: parentSpaces, DirectChildSpaces: childSpaces } = spaceData
+    const { spaceData, selectedSpaceSubPage } = useContext(SpaceContext)
+    const [parentSpaces, setParentSpaces] = useState<any[]>([])
+    const [childSpaces, setChildSpaces] = useState<any[]>([])
+    const [renderKey, setRenderKey] = useState(0)
+    const [loading, setLoading] = useState(true)
     const cookies = new Cookies()
+    const location = useLocation()
+    const spaceHandle = location.pathname.split('/')[2]
 
-    function expandSpace(type, spaceId) {
-        // todo: use local space arrays instead of mutating spaceData in context
-        const key = `Direct${type}Spaces`
-        const space = spaceData[key].find((s) => s.id === spaceId)
-        const newSpaces = [...spaceData[key]]
-        const newSpace = newSpaces.find((s) => s.id === spaceId)
+    async function getSpaces() {
+        const accessToken = cookies.get('accessToken')
+        const options = { headers: { Authorization: `Bearer ${accessToken}` } }
+        const getParentSpaces = await axios.get(
+            `${config.apiURL}/parent-spaces?spaceId=${spaceData.id}`,
+            options
+        )
+        const getChildSpaces = await axios.get(
+            `${config.apiURL}/child-spaces?spaceId=${spaceData.id}`,
+            options
+        )
+        Promise.all([getParentSpaces, getChildSpaces])
+            .then((responses) => {
+                setParentSpaces(responses[0].data)
+                setChildSpaces(responses[1].data)
+                setLoading(false)
+            })
+            .catch((error) => console.log(error))
+    }
 
-        if (space.expanded) {
+    function findSpace(spaces: any[], id: number) {
+        // traverse the tree to find matching space
+        const match = spaces.find((s) => s.id === id)
+        if (match) return match
+        for (let i = 0; i < spaces.length; i += 1) {
+            if (spaces[i].children) {
+                const childMatch = findSpace(spaces[i].children, id)
+                if (childMatch) return childMatch
+            }
+        }
+        return null
+    }
+
+    function setSpaces(type: 'parent' | 'child', spaces: any[]) {
+        if (type === 'parent') setParentSpaces(spaces)
+        else setChildSpaces(spaces)
+    }
+
+    async function expandSpace(space: any, type: 'parent' | 'child') {
+        const { id, expanded } = space
+        const spaces = type === 'parent' ? parentSpaces : childSpaces
+        const newSpaces = [...spaces]
+        const newSpace = findSpace(newSpaces, id)
+        if (expanded) {
             newSpace.expanded = false
-            setSpaceData({ ...spaceData, [key]: newSpaces })
-        } else if (space[key]) {
-            newSpace.expanded = true
-            setSpaceData({ ...spaceData, [key]: newSpaces })
+            setSpaces(type, newSpaces)
         } else {
             newSpace.loading = true
-            setSpaceData({ ...spaceData, [key]: newSpaces })
+            setSpaces(type, newSpaces)
             const accessToken = cookies.get('accessToken')
             const options = { headers: { Authorization: `Bearer ${accessToken}` } }
-            const filters =
-                'timeRange=AllTime&sortBy=Likes&sortOrder=Descending&depth=Only Direct Descendants&offset=0'
             axios
-                .get(`${config.apiURL}/space-spaces?spaceId=${spaceId}&${filters}`, options)
+                .get(`${config.apiURL}/child-spaces?spaceId=${id}`, options)
                 .then((res) => {
-                    newSpace[key] = res.data
+                    newSpace.children = res.data
                     newSpace.expanded = true
                     newSpace.loading = false
-                    setSpaceData({ ...spaceData, [key]: newSpaces })
+                    setSpaces(type, newSpaces)
+                    setRenderKey(renderKey + 1)
                 })
                 .catch((error) => console.log(error))
         }
     }
 
+    useEffect(() => {
+        if (spaceData.handle !== spaceHandle) setLoading(true)
+        else getSpaces()
+    }, [spaceData.id, location])
+
     return (
         <Scrollbars className={styles.wrapper} style={style}>
-            {parentSpaces.length > 0 && (
-                <Column className={styles.spaces}>
-                    <Row>
-                        <ArrowUpIcon />
-                        <p>Parent spaces</p>
-                    </Row>
-                    <Column>
-                        {parentSpaces.map((space) => (
-                            <Column key={space.id}>
-                                <Row centerY style={{ marginBottom: 10 }}>
-                                    <ImageTitle
-                                        type='space'
-                                        imagePath={space.flagImagePath}
-                                        title={space.name}
-                                        link={`/s/${space.handle}/${selectedSpaceSubPage}`}
-                                        fontSize={14}
-                                        imageSize={35}
-                                        onClick={() => onLocationChange && onLocationChange()}
-                                        wrapText
+            {!loading && (
+                <Column>
+                    {parentSpaces.length > 0 && (
+                        <Column className={styles.spaces}>
+                            <Row>
+                                <ArrowUpIcon />
+                                <p>Parent spaces</p>
+                            </Row>
+                            <Column key={renderKey}>
+                                {parentSpaces.map((space) => (
+                                    <Space
+                                        key={space.id}
+                                        space={space}
+                                        type='parent'
+                                        selectedSubPage={selectedSpaceSubPage}
+                                        expand={expandSpace}
+                                        onLocationChange={onLocationChange}
                                     />
-                                    <button
-                                        className={styles.expandSpaceButton}
-                                        type='button'
-                                        onClick={() => expandSpace('Parent', space.id)}
-                                    >
-                                        {space.loading ? (
-                                            <LoadingWheel size={20} style={{ marginLeft: 7 }} />
-                                        ) : (
-                                            <>{space.expanded ? <MinusIcon /> : <PlusIcon />}</>
-                                        )}
-                                    </button>
-                                </Row>
-                                {space.expanded && (
-                                    <Column scroll className={styles.childSpaces}>
-                                        {(space.DirectParentSpaces || []).map((s) => (
-                                            <Column key={s.id}>
-                                                <Row centerY style={{ marginBottom: 10 }}>
-                                                    <ImageTitle
-                                                        type='space'
-                                                        imagePath={s.flagImagePath}
-                                                        title={s.name}
-                                                        link={`/s/${s.handle}/${selectedSpaceSubPage}`}
-                                                        fontSize={14}
-                                                        imageSize={30}
-                                                        onClick={() =>
-                                                            onLocationChange && onLocationChange()
-                                                        }
-                                                        wrapText
-                                                    />
-                                                </Row>
-                                            </Column>
-                                        ))}
-                                    </Column>
-                                )}
+                                ))}
                             </Column>
-                        ))}
-                    </Column>
-                </Column>
-            )}
-            {childSpaces.length > 0 && (
-                <Column className={styles.spaces}>
-                    <Row>
-                        <ArrowDownIcon />
-                        <p>Child spaces</p>
-                    </Row>
-                    <Column>
-                        {childSpaces.map((space) => (
-                            <Column key={space.id}>
-                                <Row centerY style={{ marginBottom: 10 }}>
-                                    <ImageTitle
-                                        type='space'
-                                        imagePath={space.flagImagePath}
-                                        title={space.name}
-                                        link={`/s/${space.handle}/${selectedSpaceSubPage}`}
-                                        fontSize={14}
-                                        imageSize={35}
-                                        onClick={() => onLocationChange && onLocationChange()}
-                                        wrapText
+                        </Column>
+                    )}
+                    {childSpaces.length > 0 && (
+                        <Column className={styles.spaces}>
+                            <Row>
+                                <ArrowDownIcon />
+                                <p>Child spaces</p>
+                            </Row>
+                            <Column key={renderKey}>
+                                {childSpaces.map((space) => (
+                                    <Space
+                                        key={space.id}
+                                        space={space}
+                                        type='child'
+                                        selectedSubPage={selectedSpaceSubPage}
+                                        expand={expandSpace}
+                                        onLocationChange={onLocationChange}
                                     />
-                                    {space.totalChildren > 0 && (
-                                        <button
-                                            className={styles.expandSpaceButton}
-                                            type='button'
-                                            onClick={() => expandSpace('Child', space.id)}
-                                        >
-                                            {space.loading ? (
-                                                <LoadingWheel size={20} style={{ marginLeft: 7 }} />
-                                            ) : (
-                                                <>{space.expanded ? <MinusIcon /> : <PlusIcon />}</>
-                                            )}
-                                        </button>
-                                    )}
-                                </Row>
-                                {space.expanded && (
-                                    <Column scroll className={styles.childSpaces}>
-                                        {(space.DirectChildSpaces || []).map((s) => (
-                                            <Column key={s.id}>
-                                                <Row centerY style={{ marginBottom: 10 }}>
-                                                    <ImageTitle
-                                                        type='space'
-                                                        imagePath={s.flagImagePath}
-                                                        title={s.name}
-                                                        link={`/s/${s.handle}/${selectedSpaceSubPage}`}
-                                                        fontSize={14}
-                                                        imageSize={30}
-                                                        onClick={() =>
-                                                            onLocationChange && onLocationChange()
-                                                        }
-                                                        wrapText
-                                                    />
-                                                </Row>
-                                            </Column>
-                                        ))}
-                                    </Column>
-                                )}
+                                ))}
                             </Column>
-                        ))}
-                    </Column>
+                        </Column>
+                    )}
                 </Column>
             )}
         </Scrollbars>
