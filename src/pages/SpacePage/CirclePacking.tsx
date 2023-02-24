cec252d13bf87d7d6b2d9075ad643a326325181a
jhweir <james@weco.io>
2023-02-24 16:38:26 +0000

    CP transition fade out and attribute updates

--- src/pages/SpacePage/CirclePacking.tsx
+++ src/pages/SpacePage/CirclePacking.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 /* eslint-disable react/no-this-in-sfc */
 import { SpaceContext } from '@contexts/SpaceContext'
 import styles from '@styles/pages/SpacePage/CirclePacking.module.scss'
 import * as d3 from 'd3'
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { useHistory } from 'react-router-dom'
 
 const CirclePacking = (props: { spaceMapData: any; params: any }): JSX.Element => {
@@ -11,7 +12,8 @@ const CirclePacking = (props: { spaceMapData: any; params: any }): JSX.Element =
     const history = useHistory()
     const { sortBy, sortOrder } = params
     const mainCircleCize = 700
-    const duration = 500
+    const duration = 1000
+    const transitioning = useRef(false)
 
     const zoom = d3
         .zoom()
@@ -40,34 +42,71 @@ const CirclePacking = (props: { spaceMapData: any; params: any }): JSX.Element =
             .attr('width', '100%')
             .attr('height', '100%')
             .style('display', 'block')
-            .style('background', color(0))
         // .on('click', () => resetPosition(500))
 
         const masterGroup = svg.append('g').attr('id', 'master-group')
         masterGroup.append('g').attr('id', 'circle-group')
 
         svg.call(zoom)
-        resetPosition(0)
     }
 
-    function zoomToCircle(d) {
+    function hasMatchingAncestor(circle, selectedCircle) {
+        // recursively check parents for selectedCircle
+        if (!circle.parent) return false
+        if (circle.parent.data.id === selectedCircle.data.id) return true
+        return hasMatchingAncestor(circle.parent, selectedCircle)
+    }
+
+    function zoomToCircle(circle) {
+        transitioning.current = true
+        // navigate to new space
+        history.push(`/s/${circle.data.handle}/spaces`)
+        // transition to new circle
         const svg = d3.select('#circle-packing-svg')
         const svgWidth = parseInt(svg.style('width'), 10)
         const svgHeight = parseInt(svg.style('height'), 10)
-        const scale = mainCircleCize / (d.r * 2)
-        const x = svgWidth / 2 / scale - d.x
-        const y = svgHeight / 2 / scale - d.y
+        const scale = mainCircleCize / (circle.r * 2)
+        const x = svgWidth / 2 / scale - circle.x
+        const y = svgHeight / 2 / scale - circle.y
+        // transition circle stroke width
+        d3.selectAll('.circle')
+            .transition('circle-transition')
+            .duration(duration)
+            .attr('stroke-width', 1 / scale)
+        // transition font size
+        d3.selectAll('.text')
+            .transition('text-transition')
+            .duration(duration)
+            .attr('font-size', 16 / scale)
+            .attr('y', (da) => da.y - da.r - 15 / scale)
+        // fade out all external nodes
+        d3.selectAll('.circle,.text')
+            .filter((d) => {
+                if (d.depth <= circle.depth && d.data.id !== circle.data.id) return true
+                return d.data.id !== circle.data.id && !hasMatchingAncestor(d, circle)
+            })
+            .transition('fade-transition')
+            .duration(duration)
+            .attr('opacity', 0)
+        // zoom to new circle
         svg.transition()
             .duration(duration)
             .call(zoom.transform, d3.zoomIdentity.scale(scale).translate(x, y))
+            .on('end', () => {
+                // if (transitioning.current) {
+                //     buildTree(spaceMapData)
+                //     transitioning.current = false
+                // }
+            })
     }
 
-    function buildTree() {
+    function buildTree(data) {
+        resetPosition(0)
         const hierarchy = d3
-            .hierarchy(spaceMapData)
+            .hierarchy(data)
             .sum((d) => d.totalLikes || 1)
             .sort((a, b) => b.totalLikes - a.totalLikes)
-        const root = d3.pack().size([mainCircleCize, mainCircleCize]).padding(10)(hierarchy)
+        const root = d3.pack().size([mainCircleCize, mainCircleCize]).padding(30)(hierarchy)
 
         d3.select('#circle-group')
             .selectAll('.circle')
@@ -79,12 +118,11 @@ const CirclePacking = (props: { spaceMapData: any; params: any }): JSX.Element =
                         .classed('circle', true)
                         .attr('r', (d) => d.r)
                         .attr('stroke', '#000')
+                        .attr('stroke-width', 1)
+                        .attr('cursor', 'pointer')
                         .attr('transform', (d) => `translate(${d.x},${d.y})`)
                         .attr('fill', (d) => color(d.depth + 1))
-                        .on('click', (d) => {
-                            zoomToCircle(d)
-                            // history.push(`/s/${d.data.handle}/spaces`)
-                        })
+                        .on('click', (d) => zoomToCircle(d))
                         .call((node) => node.transition().duration(duration).attr('opacity', 1)),
                 (update) =>
                     update.call((node) =>
@@ -107,23 +145,26 @@ const CirclePacking = (props: { spaceMapData: any; params: any }): JSX.Element =
 
         d3.select('#circle-group')
             .selectAll('.text')
-            .data(root.descendants())
+            .data(root.descendants(), (d) => d.data.id)
             .join('text')
+            // .filter((d) => {
+            //     return d.depth < 2 // && d.children && d.data.totalLikes > 15
+            // })
             .classed('text', true)
             .text((d) => d.data.name)
             .attr('font-size', 16)
+            .attr('pointer-events', 'none')
             .attr('opacity', 1)
             .attr('text-anchor', 'middle')
             .attr('dominant-baseline', 'central')
             .attr('y', (d) => d.y - d.r - 15)
             .attr('x', (d) => d.x)
-            .on('click', (d) => console.log('d: ', d))
     }
 
     useEffect(() => buildCanvas(), [])
 
     useEffect(() => {
-        if (spaceMapData.id) buildTree()
+        if (spaceMapData.id) buildTree(spaceMapData)
     }, [spaceMapData])
 
     return <div id='canvas' className={styles.canvas} />
