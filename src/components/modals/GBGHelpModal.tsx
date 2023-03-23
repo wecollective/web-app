c09dd487379e4029ca0b06b416f894174018a866
jhweir <james@weco.io>
2023-03-23 23:09:55 +0000

    Old modals removed

--- src/components/modals/GBGHelpModal.tsx
+++ src/components/modals/GBGHelpModal.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable no-underscore-dangle */
 import Column from '@components/Column'
 import Modal from '@components/modals/Modal'
-import styles from '@styles/components/modals/AddPostAudioModal.module.scss'
 import React from 'react'
 
 function GBGHelpModal(props: { close: () => void }): JSX.Element {
     const { close } = props
 
     return (
-        <Modal centered close={close} className={styles.wrapper}>
+        <Modal centered close={close}>
             <h1>About The Glass Bead Game</h1>
             <Column>
                 <p>Info about game...</p>
