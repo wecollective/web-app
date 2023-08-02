6868bb24ca07b28eec53d2b9d3cbaadaaf2206c9
jhweir <james@weco.io>
2023-08-02 23:01:22 +0100

    Text trimmed on PostSpaces

--- src/components/cards/PostCard/PostSpaces.tsx
+++ src/components/cards/PostCard/PostSpaces.tsx
@@ -1,12 +1,14 @@
 import ImageTitle from '@components/ImageTitle'
-import Modal from '@components/modals/Modal'
 import Row from '@components/Row'
+import Modal from '@components/modals/Modal'
+import { trimText } from '@src/Helpers'
 import styles from '@styles/components/cards/PostCard/PostSpaces.module.scss'
 import React, { useState } from 'react'
 
 function PostSpaces(props: { spaces: any[]; preview?: boolean }): JSX.Element {
     const { spaces, preview } = props
     const [modalOpen, setModalOpen] = useState(false)
+    const mobileView = document.documentElement.clientWidth < 900
     // filter out root space 'all' and deleted spaces if included
     const filteredSpaces = spaces.filter((s) => s.id > 1 && (preview || s.state === 'active'))
 
@@ -27,7 +29,7 @@ function PostSpaces(props: { spaces: any[]; preview?: boolean }): JSX.Element {
                     type='space'
                     imagePath={filteredSpaces[0].flagImagePath}
                     imageSize={32}
-                    title={filteredSpaces[0].name}
+                    title={trimText(filteredSpaces[0].name, mobileView ? 20 : 36)}
                     fontSize={15}
                     link={preview ? null : `/s/${filteredSpaces[0].handle}/posts`}
                     style={{ margin: '0 5px' }}
