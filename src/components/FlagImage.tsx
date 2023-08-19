e90528853a53c2e4288b3fffab44929f69feddca
jhweir <james@weco.io>
2023-08-19 20:55:36 +0100

    Link prop added to FlagImage component

--- src/components/FlagImage.tsx
+++ src/components/FlagImage.tsx
@@ -2,23 +2,49 @@ import FlagImagePlaceholder from '@components/FlagImagePlaceholder'
 import { handleImageError } from '@src/Helpers'
 import styles from '@styles/components/FlagImage.module.scss'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function FlagImage(props: {
     type: 'space' | 'user' | 'post' | 'stream'
-    size: number
     imagePath: string | null
-    className?: string
+    size?: number
     outline?: boolean
     shadow?: boolean
+    link?: string | null
+    className?: string
     style?: any
 }): JSX.Element {
-    const { size, type, imagePath, className, outline, shadow, style } = props
+    const { type, imagePath, size, outline, shadow, link, className, style } = props
 
     const classes = [styles.wrapper]
     if (className) classes.unshift(className)
     if (outline) classes.push(styles.outline)
     if (shadow) classes.push(styles.shadow)
-    if (size < 50) classes.push(styles.small)
+    if (!size || size < 50) classes.push(styles.small)
+
+    if (link) {
+        return (
+            <Link
+                to={link}
+                className={classes.join(' ')}
+                style={{ width: size, height: size, ...style }}
+            >
+                {imagePath ? (
+                    <>
+                        <div className={styles.background} />
+                        <img
+                            className={styles.flagImage}
+                            src={imagePath}
+                            onError={(e) => handleImageError(e, imagePath)}
+                            alt=''
+                        />
+                    </>
+                ) : (
+                    <FlagImagePlaceholder type={type} />
+                )}
+            </Link>
+        )
+    }
 
     return (
         <div className={classes.join(' ')} style={{ width: size, height: size, ...style }}>
@@ -40,9 +66,11 @@ function FlagImage(props: {
 }
 
 FlagImage.defaultProps = {
-    className: null,
+    size: 30,
     outline: false,
     shadow: false,
+    link: null,
+    className: null,
     style: null,
 }
 
