e17cdc135ed18aa9ae3792da9b3776153a1d244c
jhweir <james@weco.io>
2023-12-29 20:17:12 +0000

    PostSpaces preview update

--- src/components/cards/PostCard/PostSpaces.tsx
+++ src/components/cards/PostCard/PostSpaces.tsx
@@ -1,6 +1,7 @@
 import Row from '@components/Row'
 import SpaceButton from '@components/SpaceButton'
 import Modal from '@components/modals/Modal'
+import ImageTitle from '@src/components/ImageTitle'
 import styles from '@styles/components/cards/PostCard/PostSpaces.module.scss'
 import React, { useState } from 'react'
 
@@ -23,41 +24,64 @@ function PostSpaces(props: { spaces: any[]; size?: number; preview?: boolean }):
     return (
         <Row centerY className={styles.wrapper}>
             <p className='grey'>to</p>
-            {filteredSpaces[0] && (
-                <SpaceButton
-                    space={filteredSpaces[0]}
-                    imageSize={size}
-                    fontSize={15}
-                    maxChars={mobileView ? 20 : 36}
-                    style={{ margin: '0 5px' }}
-                    shadow
-                />
-            )}
-            {filteredSpaces[1] && (
-                <button
-                    type='button'
-                    className={styles.otherSpacesButton}
-                    title={otherSpacesTitle()}
-                    onClick={() => setModalOpen(true)}
-                    style={{ width: size, height: size }}
-                >
-                    <p>+{filteredSpaces.length - 1}</p>
-                </button>
-            )}
-            {modalOpen && (
-                <Modal centerX close={() => setModalOpen(false)}>
-                    <h2>Posted to</h2>
-                    {filteredSpaces.map((space) => (
-                        <SpaceButton
-                            key={space.id}
-                            space={space}
+            {preview ? (
+                <>
+                    {filteredSpaces[0] && (
+                        <ImageTitle
+                            type='space'
+                            imagePath={filteredSpaces[0].flagImagePath}
+                            title={filteredSpaces[0].name}
                             imageSize={size}
                             fontSize={15}
-                            style={{ marginTop: 10 }}
+                            style={{ margin: '0 5px' }}
+                        />
+                    )}
+                    {filteredSpaces[1] && (
+                        <div className={styles.otherSpaces} style={{ width: size, height: size }}>
+                            <p>+{filteredSpaces.length - 1}</p>
+                        </div>
+                    )}
+                </>
+            ) : (
+                <>
+                    {filteredSpaces[0] && (
+                        <SpaceButton
+                            space={filteredSpaces[0]}
+                            imageSize={size}
+                            fontSize={15}
+                            maxChars={mobileView ? 20 : 36}
+                            style={{ margin: '0 5px' }}
                             shadow
                         />
-                    ))}
-                </Modal>
+                    )}
+                    {filteredSpaces[1] && (
+                        <button
+                            type='button'
+                            className={`${styles.otherSpaces} ${styles.button}`}
+                            title={otherSpacesTitle()}
+                            onClick={() => setModalOpen(true)}
+                            disabled={preview}
+                            style={{ width: size, height: size }}
+                        >
+                            <p>+{filteredSpaces.length - 1}</p>
+                        </button>
+                    )}
+                    {modalOpen && (
+                        <Modal centerX close={() => setModalOpen(false)}>
+                            <h2>Posted to</h2>
+                            {filteredSpaces.map((space) => (
+                                <SpaceButton
+                                    key={space.id}
+                                    space={space}
+                                    imageSize={size}
+                                    fontSize={15}
+                                    style={{ marginTop: 10 }}
+                                    shadow
+                                />
+                            ))}
+                        </Modal>
+                    )}
+                </>
             )}
         </Row>
     )
