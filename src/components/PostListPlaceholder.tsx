9cd306e42d43bf43a1c3008e89fe6a0a03a9dfa0
jhweir <james@weco.io>
2024-01-18 19:49:02 +0000

    PostList placeholder updates

--- src/components/PostListPlaceholder.tsx
+++ src/components/PostListPlaceholder.tsx
@@ -3,10 +3,14 @@ import Placeholder from '@components/cards/PostCard/Placeholder'
 import styles from '@styles/components/ListPlaceholder.module.scss'
 import React from 'react'
 
-function PostListPlaceholder(): JSX.Element {
+function PostListPlaceholder(props: { id: string }): JSX.Element {
+    const { id } = props
     return (
-        <Column className={styles.wrapper}>
-            <div className={styles.gradient} />
+        <Column className={styles.wrapper} id={id}>
+            {/* <div className={styles.gradient} /> */}
+            <Placeholder />
+            <Placeholder />
+            <Placeholder />
             <Placeholder />
             <Placeholder />
             <Placeholder />
